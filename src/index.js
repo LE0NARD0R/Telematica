eda83740a90a2a352b718be70c048fa12185e9b0
LE0NARD0R <laalvear@uninorte.edu.co>
2022-11-22 13:47:42 -0500

    First adding backend to the page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const path =require("path");
 
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host:   "localhost",
     user:   "root", 
     password: "123456789",
@@ -13,4 +13,14 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("conectado");
+});
+
+//settings
+app.set('port', process.env.PORT || 9500)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), () => {
+    console.log('server on port 9500')
 });
